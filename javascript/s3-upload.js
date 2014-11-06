b7e5750f46e5f169140d6f46f895ab6c08928b36
Chris Roos <chris@seagul.co.uk>
2014-11-06 17:10:50 +0000

    Make uploadFile a global function
    So that I can eventually switch between single file and multipart in
    the browser.

--- javascript/s3-upload.js
+++ javascript/s3-upload.js
@@ -1,24 +1,24 @@
-$(function() {
-  var uploadFile = function(file) {
-    start();
+var uploadFile = function(file) {
+  start();
 
-    var params = {
-      'Key': file.name,
-      'ContentType': file.type,
-      'Body': file
-    };
-
-    window.bucket.putObject(params, function(err, data) {
-      finish();
-
-      if (err) {
-        log('Error!');
-      } else {
-        log('Uploaded successfully.');
-      };
-    });
+  var params = {
+    'Key': file.name,
+    'ContentType': file.type,
+    'Body': file
   };
 
+  window.bucket.putObject(params, function(err, data) {
+    finish();
+
+    if (err) {
+      log('Error!');
+    } else {
+      log('Uploaded successfully.');
+    };
+  });
+};
+
+$(function() {
   window.button.addEventListener('click', function() {
     var file = window.fileChooser.files[0];
     if (file) {
